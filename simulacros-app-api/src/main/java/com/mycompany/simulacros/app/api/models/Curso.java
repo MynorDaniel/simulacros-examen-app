f755977f801eb6dbcb8135a2267142464b890c97
Mynor Morales <mynordma@gmail.com>
2024-10-17 15:40:31 -0600

    endpoint curso

---
+++ simulacros-app-api/src/main/java/com/mycompany/simulacros/app/api/models/Curso.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.simulacros.app.api.models;
+
+/**
+ *
+ * @author mynordma
+ */
+public class Curso {
+    private String nombre;
+    private String carrera;
+    private String division;
+    private String descripcion;
+
+    public Curso(String nombre, String carrera, String division, String descripcion) {
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.division = division;
+        this.descripcion = descripcion;
+    }
+    
+    public Curso(){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
---
+++ simulacros-app-api/src/main/java/com/mycompany/simulacros/app/api/resources/CursoResource.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.simulacros.app.api.resources;
+
+import com.mycompany.simulacros.app.api.models.Curso;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+/**
+ *
+ * @author nombre
+ */
+@Path("curso")
+public class CursoResource {
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crearCurso(Curso curso){
+        
+        return null;
+        
+    }
+}
