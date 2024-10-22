b2ffc6b5dc543d6de0de1c659b227e60632878f8
Cris <ymcrisystems@gmail.com>
2024-10-21 21:43:13 -0600

    Implementando metodo obtenerCursos

--- simulacros-app-api/src/main/java/com/mycompany/simulacros/app/api/controllers/CursoController.java
+++ simulacros-app-api/src/main/java/com/mycompany/simulacros/app/api/controllers/CursoController.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.simulacros.app.api.controllers;
 
+import com.mycompany.simulacros.app.api.models.Curso;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author mynordma
  */
 public class CursoController {
+
+    //Confio en que el segundo método sea el correcto, pero por si acaso puse 2.
     
+    /*
+    public Curso[] obtenerCursos() {
+        try {
+            ArrayList<Curso> listaDeCursos = CursoDB.obtenerCursos();
+            Curso[] cursos = (Curso[]) listaDeCursos.toArray();
+            return cursos;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+    */
+ 
+    /*
+    public Curso[] obtenerCursos() {
+        try {
+            CursoDB cursoDB = new CursoDB();
+            Curso[] cursos = (Curso[]) cursoDB.obtenerCursos().toArray();
+            return cursos;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+    */
 }
